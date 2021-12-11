0637cd6a5eb439755fba555375d9492c5223dec3
Wes Wollner <weswollner@gmail.com>
2021-12-11 15:34:27 -0500

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "e90testprojectb7ef3cab": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header>
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>We now have Auth!</h1>
+//       </header>
+//       <AmplifySignOut />
+//     </div>
+//   );
+// }
+
+// export default withAuthenticator(App);
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <p>
+            Hey {user.username}, welcome, with auth!
+          </p>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
